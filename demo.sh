fe3b4b1415803f0573c8e0e2a07933cb151ede6a
Gloria Ciavarrini <gciavarrini@redhat.com>
2025-03-06 17:39:37 +0100

    Minor improvements
    Signed-off-by: Gloria Ciavarrini <gciavarrini@redhat.com>

--- demo.sh
+++ demo.sh
@@ -7,7 +7,7 @@ source demo-magic.sh
 DEMO_PROMPT="➜ "
 
 DEMO_PROMPT="${GREEN}➜ ${CYAN}\W ${COLOR_RESET}"
-TYPE_SPEED=15
+TYPE_SPEED=8
 PROMPT_TIMEOUT=3
 DEMO_COMMENT_COLOR=$PURPLE
 SHOW_CMD_NUMS=true
@@ -28,15 +28,15 @@ comment "# Kantra CLI - Asset Generation Demo",
 comment "# This demo shows how to use 'discover' and 'generate' subcommands."
 wait
 
-comment "\n#  Clone the Kantra repository since the functionality is still in a PR."
-pei 'git clone git@github.com:konveyor/kantra.git /tmp/kantra'
-pei 'cd /tmp/kantra'
+# comment "\n#  Clone the Kantra repository since the functionality is still in a PR."
+git clone -q git@github.com:konveyor/kantra.git /tmp/kantra 
+cd /tmp/kantra 
 
-comment "\n# Checkout the asset generation PR."
-pei 'gh pr checkout 435'
+# comment "\n# Checkout the asset generation PR."
+gh pr checkout 435 &> /dev/null
 
-comment "\n# Build the project"
-pei 'go build'
+# comment "\n# Build the project"
+go build &> /dev/null
 
 comment "\n#  Let's focus on discover subcommand"
 pei './kantra discover -h'
@@ -58,12 +58,13 @@ comment "\n#  Let's use a sample one!"
 pei 'cat ./cf-nodejs-app.yaml'
 wait
 
-comment "\n# After executing the discover command, we expect that the command prints the canonical manifest."
+comment "\n\n# After executing the discover command, we expect that the command prints the canonical manifest."
 pei './kantra discover cloud-foundry --input ./cf-nodejs-app.yaml'
 wait
 
 comment "\n# ... But we can also write the output on a file"
 pei './kantra discover cloud-foundry --input ./cf-nodejs-app.yaml --output cf-nodejs-app-canonical.yaml'
+wait 
 
 pei 'cat  cf-nodejs-app-canonical.yaml'
 wait
@@ -74,6 +75,7 @@ wait
 
 comment "\n# We only support helm at the moment"
 pei './kantra generate helm -h'
+wait 
 
 cp -r $DEMO_DIR/helm_sample /tmp/kantra/helm_sample
 
@@ -86,14 +88,24 @@ wait
 pei "cat helm_sample/templates/configmap.yaml"
 wait
 
-comment "\n# Now, let's use the canonical manifest created by the 'discover' command (step n.17)"
-comment "#to generate Helm template manifest."
-pei "./kantra generate helm --chart-dir helm_sample --input cf-nodejs-app-canonical.yaml"
+comment "\n# There is also a Dockerfile (non-k8s manifest)."
+pei "cat helm_sample/files/konveyor/Dockerfile"
+wait
 
-comment "\n# You can also store the result in a file"
+comment "\n# Now, let's use the canonical manifest created by the 'discover' command (step n.17)"
+comment "# to generate Helm template manifest."
+pei "./kantra generate helm --chart-dir helm_sample --input cf-nodejs-app-canonical.yaml"
+wait 
+
+comment "\n# You can also store the resuls in a directory"
 pei "./kantra generate helm --chart-dir helm_sample --input cf-nodejs-app-canonical.yaml --output-dir outDir"
 wait
 
 pei "ls outDir"
+comment "\n# This is the generated configmap."
 pei "cat outDir/configmap.yaml"
+wait
+
+comment "\n# This is the generated Dockerfile."
+pei "cat outDir/Dockerfile"
 wait
